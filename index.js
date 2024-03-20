87a89e9f6fd1ab2e66823bbe26df64820a75205c
Victorien Garrigues <v1c7oga22@gmail.com>
2024-03-20 12:40:08 -0400

    new style

--- index.js
+++ index.js
@@ -1,55 +1,3 @@
-// window.addEventListener("scroll", reveal);
-
-// // To check the scroll position on page load
-// reveal(".num");
-
-
-// // let valueDisplays = document.querySelectorAll(".num");
-// // let interval = 4000;
-// // valueDisplays.forEach((valueDisplay) => {
-// //   let startValue = 0;
-// //   let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-// //   let duration = Math.floor(interval / endValue);
-// //   let counter = setInterval(function () {
-// //     startValue += 1;
-// //     valueDisplay.textContent = startValue;
-// //     if (startValue == endValue) {
-// //       clearInterval(counter);
-// //     }
-// //   }, duration);
-// // });
-
-
-
-// const handleOnMouseMove = e => {
-//     const { currentTarget: target } = e;
-
-//     const rect = target.getBoundingClientRect(),
-//         x = e.clientX - rect.left,
-//         y = e.clientY - rect.top;
-
-//     target.style.setProperty("--mouse-x", Math.floor(x).toString().concat("px"));
-//     target.style.setProperty("--mouse-y", Math.floor(y).toString().concat("px"));
-// }
-
-// for(const card of document.querySelectorAll(".animationCard")) {
-//     console.log(card);
-//     card.onmousemove = e => handleOnMouseMove(e);
-// }
-
-// function reveal(element) {
-//     var reveals = document.querySelectorAll(element);
-//     for (var i = 0; i < reveals.length; i++) {
-//       var windowHeight = window.innerHeight;
-//       var elementTop = reveals[i].getBoundingClientRect().top;
-//       var elementVisible = 150;
-//       if (elementTop < windowHeight - elementVisible) {
-//         reveals[i].classList.add("active");
-//       } else {
-//         reveals[i].classList.remove("active");
-//       }
-//     }
-//   }
 var isExpanded = false;
 const myNav = document.getElementById('navbar');
 const navEx = document.getElementById('navLinksEx');
@@ -119,7 +67,7 @@ function toggleFaq(name) {
     document.getElementById(name).classList.remove("toggled");
     document.getElementById(name).style.paddingTop = '40px';
     document.getElementById(name).style.transform = 'translateY(-20px)';
-    document.getElementById(name).style.height = 'fit-content';
+    // document.getElementById(name).style.height = 'fit-content';
     document.getElementById(name).style.borderRadius = '0 0 20px 20px';
     document.getElementById(name).style.marginBottom = '50px';
     document.getElementById(plusName).style.transform = 'rotate(45deg)';
